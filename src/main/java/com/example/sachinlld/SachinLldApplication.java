38741ca3db19c081f0ecbb83b7972cbf4b7ed800
Sachin Tiwari <sachin@changejar.in>
2025-04-13 16:47:49 +0530

    Start LLD

---
+++ src/main/java/com/example/sachinlld/SachinLldApplication.java
@@ -0,0 +1,13 @@
+package com.example.sachinlld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SachinLldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SachinLldApplication.class, args);
+    }
+
+}
